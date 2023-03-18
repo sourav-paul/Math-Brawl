8216147be1bb6fa3625a5700c87bb4f923fbeb16
Sourav <srvpul@gmail.com>
2023-03-18 22:39:41 +0100

    more websocket server code

--- Math Brawl Net Server/MathBrawlServer/MathBrawlServer/Startup.cs
+++ Math Brawl Net Server/MathBrawlServer/MathBrawlServer/Startup.cs
@@ -8,6 +8,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Net.WebSockets;
+using System.Threading;
+using IHostingEnvironment = Microsoft.AspNetCore.Hosting.IHostingEnvironment;
 
 namespace MathBrawlServer
 {
@@ -27,12 +29,67 @@ namespace MathBrawlServer
                 {
                     WebSocket webSocket = await context.WebSockets.AcceptWebSocketAsync();
                     Console.WriteLine("WebSocket Connected");
+                    
+                    await ReceiveMessage(webSocket, async (result, buffer) =>
+                    {
+                        if (result.MessageType == WebSocketMessageType.Text)
+                        {
+                            Console.WriteLine($"Receive->Text");
+
+                            return;
+                        }
+                        else if (result.MessageType == WebSocketMessageType.Close)
+                        {
+                            Console.WriteLine($"Receive->Close");
+
+                            return;
+                        }
+                    });
+                    
                 }
                 else
                 {
+                    //Add a Console.WriteLine to see when we move to next Delegate
+                    Console.WriteLine("Hello from 2nd Request Delegate - No WebSocket");
                     await next();
                 }
             });
+            
+            app.Run(async context => 
+            {
+                Console.WriteLine("Hello from 3rd (terminal) Request Delegate");
+                await context.Response.WriteAsync("Hello from 3rd (terminal) Request Delegate");
+            });
+        }
+
+        public void WriteRequestParam(HttpContext context, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                Console.WriteLine("Request Method: " + context.Request.Method);
+                Console.WriteLine("Request Protocol: " + context.Request.Protocol);
+
+                if (context.Request.Headers != null)
+                {
+                    Console.WriteLine("Request Headers: ");
+                    foreach (var h in context.Request.Headers)
+                    {
+                        Console.WriteLine("--> " + h.Key + ": " + h.Value);
+                    }
+                }
+            }
+        }
+        
+        private async Task ReceiveMessage(WebSocket socket, Action<WebSocketReceiveResult, byte[]> handleMessage)
+        {
+            var buffer = new byte[1024 * 4];
+
+            while (socket.State == WebSocketState.Open)
+            {
+                var result = await socket.ReceiveAsync(buffer: new ArraySegment<byte>(buffer),
+                    cancellationToken: CancellationToken.None);
+                handleMessage(result, buffer);
+            }
         }
     }
 }
